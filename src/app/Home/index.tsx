840965914c93897ed394023610cce11712f427d7
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-07-01 10:17:32 -0300

    feat: Initial structure to content

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -7,8 +7,13 @@ export function Home(){
   return(
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo}/>
-      <Input placeholder="O que você precisa comprar"/>
-      <Button title="Adicionar"/>
+
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar"/>
+        <Button title="Adicionar"/>
+      </View>
+
+      <View style={styles.content}></View>
     </View>
   )
 }
