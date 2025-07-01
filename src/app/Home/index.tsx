ad77839a68238b48eb9acfedc73636ef6c1419d8
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-07-01 10:08:56 -0300

    feat: Create Input component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,12 +1,13 @@
 import { View, Image } from "react-native";
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 import { styles } from "./styles";
 
 export function Home(){
   return(
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo}/>
-
+      <Input placeholder="O que você precisa comprar"/>
       <Button title="Adicionar"/>
     </View>
   )
