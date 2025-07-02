eb6363da0d65d68dc4186b84c23a6ac71f5a5c55
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-07-02 09:16:20 -0300

    feat: Create header to content

--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -26,5 +26,21 @@ export const styles = StyleSheet.create({
     borderTopRightRadius: 24,
     padding: 24,
     marginTop: 24,
+  },
+  header:{
+    width:"100%",
+    flexDirection: "row",
+    gap: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E4E6EC",
+    paddingBottom: 12,
+  },
+  clearButton:{
+    marginLeft: "auto",
+  },
+  clearText:{
+    fontSize: 12,
+    color: "#828282",
+    fontWeight: 600
   }
 })
